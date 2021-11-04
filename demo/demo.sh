479317b043575106cdac94f70f190b580d5c18dd
Alice Frosi <afrosi@redhat.com>
2021-11-04 11:23:02 +0100

    Fix demo to run encryption image task

--- demo/demo.sh
+++ demo/demo.sh
@@ -5,6 +5,7 @@ export KUBECONFIG=~/.crc/machines/crc/kubeconfig
 LOCAL_IMAGE=localhost/image-tekton-encrypt:latest
 IS=tekton-encrypt-images
 NS=encrypt-image-demo
+SA=tekton-encryp-images
 # Push into the local registry
 oc login -u kubeadmin -p $(cat ~/.crc/machines/crc/kubeadmin-password) https://api.crc.testing:6443
 oc patch configs.imageregistry.operator.openshift.io/cluster --patch '{"spec":{"defaultRoute":true}}' --type=merge
@@ -17,7 +18,14 @@ podman push --tls-verify=false $IMAGE
 
 # Create the tekton task
 oc new-project $NS
-oc create -f ../tekton-task.yaml
+
+# Create sa for tekton task
+oc apply -f ../security-context.yaml
+oc create sa $SA
+oc adm policy add-scc-to-user scc-admin-demo  system:serviceaccount:$NS:$SA
+
+
+oc apply -f ../tekton-task.yaml
 # Run the tekton task
-oc create -f ../task-run.yaml
+oc apply -f ../task-run.yaml
 
