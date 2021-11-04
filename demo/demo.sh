313feae6fab88fc6fc8b5755ebcda6ff2ba4f788
Alice Frosi <afrosi@redhat.com>
2021-11-04 08:50:50 +0100

    Fix namespace in the demo

--- demo/demo.sh
+++ demo/demo.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-set -xe
+set -x
 export KUBECONFIG=~/.crc/machines/crc/kubeconfig
 LOCAL_IMAGE=localhost/image-tekton-encrypt:latest
 IS=tekton-encrypt-images
-
+NS=encrypt-image-demo
 # Push into the local registry
 oc login -u kubeadmin -p $(cat ~/.crc/machines/crc/kubeadmin-password) https://api.crc.testing:6443
 oc patch configs.imageregistry.operator.openshift.io/cluster --patch '{"spec":{"defaultRoute":true}}' --type=merge
 HOST=$(oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
 podman login -u kubeadmin -p $(oc whoami -t) --tls-verify=false $HOST 
 oc create imagestream $IS
-podman tag $LOCAL_IMAGE $HOST/$IS:latest
+IMAGE=$HOST/$NS/$IS:latest
+podman tag $LOCAL_IMAGE $IMAGE
+podman push --tls-verify=false $IMAGE
 
 # Create the tekton task
-oc new-project encrypt-image-demo
+oc new-project $NS
 oc create -f ../tekton-task.yaml
 # Run the tekton task
 oc create -f ../task-run.yaml
